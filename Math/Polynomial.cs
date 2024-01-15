dc02f5c1d13e7e4bbe9e54aef2104b7492187a3a
BlokerX <67635977+BlokerX@users.noreply.github.com>
2024-01-15 16:05:33 +0100

    Polynomial.

---
+++ Math/Polynomial.cs
@@ -0,0 +1,40 @@
+﻿namespace BlokMath
+{
+    public class Polynomial
+    {
+        public Polynomial(float[] a)
+        {
+            this.a = a;
+        }
+
+        // [0]x^4 [1]x^3 [2]x^2 [3]x [4]x^0 (the last one equals 1)
+        public float[] a { get; private set; }
+
+        public float f(float x)
+        {
+            float temp = 0;
+            float tempX = 1;
+            for (int i = a.Length - 1; i < 0; i--)
+            {
+                temp += a[i] * tempX;
+                tempX *= x;
+            }
+            return temp;
+        }
+
+        public Polynomial Derivative()
+        {
+            if (a.Length == 1)
+                return new Polynomial([0]);
+
+            int tempXPow = a.Length - 1;
+            float[] tab = new float[a.Length - 1];
+            for (int i=0; i < tab.Length; i++)
+            {
+                tab[i] = a[i] * tempXPow;
+                tempXPow--;
+            }
+            return new Polynomial(tab);
+        }
+    }
+}

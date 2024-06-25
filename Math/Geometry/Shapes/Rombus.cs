503b22f941eb9c0b1c16bf46cd188e1f6a88332a
BlokerX <67635977+BlokerX@users.noreply.github.com>
2024-06-25 16:38:29 +0200

    Rombus but with bugs.

--- Math/Geometry/Shapes/Rombus.cs
+++ Math/Geometry/Shapes/Rombus.cs
@@ -1,16 +1,128 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlokMath.Functions;
 
 namespace BlokMath.Geometry.Shapes
 {
-    internal class Rombus
+    public class Rombus
     {
+        public Rombus(decimal e, decimal f)
+        {
+            this.e = e;
+            this.f = f;
+
+            Set_e_Or_f();
+        }
+
+        //public Rombus(decimal a, decimal h)
+        //{
+
+        //}
+
         public decimal a { get; private set; }
+
+        public decimal h { get; private set; }
 
         public decimal e { get; private set; }
         public decimal f { get; private set; }
+
+        public decimal alpha
+        {
+            get
+            {
+                // Przekątne dzielą romb na cztery trójkąty prostokątne
+                decimal half_e = e / 2;
+                decimal half_f = f / 2;
+
+                // Oblicz kąt alpha używając arctangens
+                // Zamieniamy wyniki z radianów na stopnie
+                double alphaRad = Math.Atan((double)(half_f / half_e));
+                return (decimal)(alphaRad * (180.0 / Math.PI));
+            }
+        }
+
+        public decimal beta
+        {
+            get
+            {
+                // Przekątne dzielą romb na cztery trójkąty prostokątne
+                decimal half_e = e / 2;
+                decimal half_f = f / 2;
+
+                // Oblicz kąt beta używając arctangens
+                // Zamieniamy wyniki z radianów na stopnie
+                double alphaRad = Math.Atan((double)(half_e / half_f));
+                return (decimal)(alphaRad * (180.0 / Math.PI));
+            }
+        }
+
+        public void Set_a(decimal a)
+        {
+            this.a = a;
+            Set_a_Or_h(a);
+        }
+
+        public void Set_h(decimal h)
+        {
+            this.h = h;
+            Set_a_Or_h(a);
+        }
+
+        private void Set_a_Or_h(decimal a)
+        {
+            #region e_Oraz_f
+
+            // Oblicz e * f
+            decimal ef = 2 * a * h;
+
+            // Oblicz e^2 + f^2
+            decimal e2_plus_f2 = 4 * a * a;
+
+            // Rozwiązujemy układ równań
+            // e = sqrt((e2_plus_f2 + sqrt(e2_plus_f2^2 - 4 * ef^2)) / 2)
+            // f = ef / e
+
+            decimal discriminant = OtherFunctions.SquareRoot(e2_plus_f2 * e2_plus_f2 - 4 * ef * ef);
+            e = OtherFunctions.SquareRoot((e2_plus_f2 + discriminant) / 2);
+            f = ef / e;
+
+            #endregion
+        }
+
+        public void Set_e(decimal e)
+        {
+            this.e = e;
+            Set_e_Or_f();
+        }
+
+        public void Set_f(decimal f)
+        {
+            this.f = f;
+            Set_e_Or_f();
+        }
+
+        private void Set_e_Or_f()
+        {
+            a = OtherFunctions.SquareRoot(OtherFunctions.Power(e / 2, 2) + OtherFunctions.Power(f / 2, 2));
+            h = ((e * f) / 2) / a;
+        }
+
+        //public void Set_alpha(decimal alpha)
+        //{
+        //    this.alpha = alpha;
+        //}
+        //public void Set_beta(decimal beta)
+        //{
+        //    this.beta = beta;
+        //}
+
+        public void Set_Circumference(decimal circumference)
+        {
+            if (circumference != 0)
+                a = circumference / 4;
+            else a = 0;
+        }
+
+        public decimal Area => a * h; // todo (e*f)/2
+
+        public decimal Circumference => a * 4;
     }
 }
---
+++ SuperCalculatorMauiApp/Controls/RombusView.xaml.cs
@@ -0,0 +1,106 @@
+﻿using BlokMath.Geometry.Shapes;
+
+namespace SuperCalculatorMauiApp.Controls
+{
+    public partial class RombusView : ContentView
+    {
+        public RombusView()
+        {
+            InitializeComponent();
+            UpdateObjectValues();
+        }
+
+        private void UpdateObjectValues()
+        {
+            isChanging = true;
+
+            a_Entry.Text = _robmus.a.ToString();
+
+            h_Entry.Text = _robmus.h.ToString();
+
+            e_Entry.Text = _robmus.e.ToString();
+            f_Entry.Text = _robmus.f.ToString();
+
+            alpha_Entry.Text = _robmus.alpha.ToString();
+            beta_Entry.Text = _robmus.beta.ToString();
+
+            isChanging = false;
+        }
+
+        private bool isChanging = false;
+
+        private Rombus _robmus = new Rombus(1, 1);
+
+        private void a_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _robmus.Set_a(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void h_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _robmus.Set_h(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void e_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _robmus.Set_e(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void f_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _robmus.Set_f(result);
+                UpdateObjectValues();
+            }
+        }
+
+        //private void alpha_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    if (isChanging)
+        //        return;
+
+        //    if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+        //    {
+        //        _robmus.Set_alpha(result);
+        //        UpdateObjectValues();
+        //    }
+        //}
+
+        //private void beta_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        //{
+        //    if (isChanging)
+        //        return;
+
+        //    if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+        //    {
+        //        _robmus.Set_beta(result);
+        //        UpdateObjectValues();
+        //    }
+        //}
+    }
+}

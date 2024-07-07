62087dc9ef26300d2b3c343688d72de5189901fe
BlokerX <67635977+BlokerX@users.noreply.github.com>
2024-07-07 13:45:26 +0200

    Sphere but I found bugs in sphere and circle.

--- Math/Geometry/Solids/Sphere.cs
+++ Math/Geometry/Solids/Sphere.cs
@@ -15,7 +15,37 @@ namespace BlokMath.Geometry.Solids
         public Circle CircleCrossSection { get; private set; }
         public decimal r { get => CircleCrossSection.r; }
 
-        public decimal SurfaceArea() => 4 * OtherFunctions.PI * r * r;
-        public decimal Volume() => r * r * 4 / 3 * OtherFunctions.PI;
+        public decimal SurfaceArea => 4 * OtherFunctions.PI * r * r;
+        public decimal Volume => r * r * r * (4 / 3) * OtherFunctions.PI;
+
+        public void Set_r(decimal r)
+        {
+            CircleCrossSection?.Set_r(r);
+        }
+
+        public void Set_d(decimal d)
+        {
+            CircleCrossSection?.Set_d(d);
+        }
+
+        public void Set_CircleCrossSectionCircumference(decimal circumference)
+        {
+            CircleCrossSection?.Set_Circumference(circumference);
+        }
+
+        public void Set_CircleCrossSectionArea(decimal area)
+        {
+            CircleCrossSection?.Set_Area(area);
+        }
+
+        public void Set_SurfaceArea(decimal surfaceArea)
+        {
+            CircleCrossSection?.Set_r(OtherFunctions.SquareRoot(surfaceArea / (4 * OtherFunctions.PI)));
+        }
+
+        public void Set_Volume(decimal volume)
+        {
+            CircleCrossSection?.Set_r(OtherFunctions.CubeRoot((3 * volume) / (4 * OtherFunctions.PI)));
+        }
     }
 }
---
+++ SuperCalculatorMauiApp/Controls/SphereView.xaml.cs
@@ -0,0 +1,102 @@
+﻿using BlokMath.Geometry.Solids;
+
+namespace SuperCalculatorMauiApp.Controls
+{
+    public partial class SphereView : ContentView
+    {
+        public SphereView()
+        {
+            InitializeComponent();
+            // set default values
+            UpdateObjectValues();
+        }
+
+        private void UpdateObjectValues()
+        {
+            isChanging = true;
+            r_Entry.Text = _sphere.CircleCrossSection.r.ToString();
+            d_Entry.Text = _sphere.CircleCrossSection.d.ToString();
+            SurfaceArea_Entry.Text = _sphere.SurfaceArea.ToString();
+            Volume_Entry.Text = _sphere.Volume.ToString();
+            CircleCrossSectionCircumference_Entry.Text = _sphere.CircleCrossSection.Circumference.ToString();
+            CircleCrossSectionArea_Entry.Text = _sphere.CircleCrossSection.Area.ToString();
+            isChanging = false;
+        }
+
+        private bool isChanging = false;
+
+        private Sphere _sphere = new Sphere(1);
+
+        private void r_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _sphere.Set_r(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void d_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _sphere.Set_d(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void Volume_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _sphere.Set_Volume(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void SurfaceArea_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _sphere.Set_SurfaceArea(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void CircleCrossSectionCircumference_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _sphere.Set_CircleCrossSectionCircumference(result);
+                UpdateObjectValues();
+            }
+        }
+
+        private void CircleCrossSectionArea_Entry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (isChanging)
+                return;
+
+            if (decimal.TryParse(((Entry)sender).Text, out decimal result))
+            {
+                _sphere.Set_CircleCrossSectionArea(result);
+                UpdateObjectValues();
+            }
+        }
+    }
+}

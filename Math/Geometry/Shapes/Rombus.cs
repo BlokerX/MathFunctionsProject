b95df5a242e348bb3c2ac10a773b61655a67431b
BlokerX <67635977+BlokerX@users.noreply.github.com>
2024-07-07 12:09:52 +0200

    Rombus fixes.

--- Math/Geometry/Shapes/Rombus.cs
+++ Math/Geometry/Shapes/Rombus.cs
@@ -57,17 +57,26 @@ namespace BlokMath.Geometry.Shapes
         public void Set_a(decimal a)
         {
             this.a = a;
-            Set_a_Or_h(a);
+            Set_a_Or_h();
         }
 
         public void Set_h(decimal h)
         {
             this.h = h;
-            Set_a_Or_h(a);
+            Set_a_Or_h();
         }
 
-        private void Set_a_Or_h(decimal a)
+        private void Set_a_Or_h()
         {
+            // Sprawdzenie, czy podane wartości są poprawne
+            if (a <= 0 || h <= 0 || h >= 2 * a)
+            {
+#if DEBUG
+                //todo throw new ArgumentException("Podane wartości boku i wysokości są nieprawidłowe.");
+#endif
+                return;
+            }
+
             #region e_Oraz_f
 
             // Oblicz e * f
@@ -101,6 +110,15 @@ namespace BlokMath.Geometry.Shapes
 
         private void Set_e_Or_f()
         {
+            // Sprawdzenie, czy podane wartości są poprawne
+            if (e <= 0 || f <= 0)
+            {
+#if DEBUG
+                //todo throw new ArgumentException("Podane wartości przekątnych są nieprawidłowe.");
+#endif
+                return;
+            }
+
             a = OtherFunctions.SquareRoot(OtherFunctions.Power(e / 2, 2) + OtherFunctions.Power(f / 2, 2));
             h = ((e * f) / 2) / a;
         }
